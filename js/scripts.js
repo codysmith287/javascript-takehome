531399a6e1da7bc0e13902260e229c17b2e1e49f
Cody Smith <cms287@Codys-MacBook-Pro.local>
2018-07-08 20:44:58 -0700

    take home complete :)

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let firstName = prompt("What is your first name?", "John");
+let lastName = prompt("What is your last name?", "Smith");
+console.log(firstName);
+alert (lastName + " is a cool last name!");
+let birthday = new Date(prompt("What is your birthday? (MM/DD/YYYY)"));
+
+ if (confirm (firstName + " " + lastName + ", are you sure " + birthday + " is your correct birthday?")) {
+    alert ("Hurray! " + birthday + " is a historic day in which an awesome person was born!");
+ } else {
+    alert ("Please reload the web page and enter the correct date when prompted.")
+ };
