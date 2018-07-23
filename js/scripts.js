e06dce8a3cdb67554105251a56bf5cfb2bd31702
Cody Smith <cms287@gmail.com>
2018-07-22 20:17:21 -0700

    takehome day 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,51 @@
-/* Takehome Day 2
+// 1. Take a number and return the square of that number (power of 2).
 
-let firstName = prompt("What is your first name?", "John");
-let lastName = prompt("What is your last name?", "Smith");
-console.log(firstName);
-alert (lastName + " is a cool last name!");
-let birthday = new Date(prompt("What is your birthday? (MM/DD/YYYY)"));
+let answer = numSquared(parseInt(prompt("What number do you want to square?", "2")));
 
- if (confirm (firstName + " " + lastName + ", are you sure " + birthday + " is your correct birthday?")) {
-    alert ("Hurray! " + birthday + " is a historic day in which an awesome person was born!");
- } else {
-    alert ("Please reload the web page and enter the correct date when prompted.")
- };
 
- End of Takehome Day 2 */
+function numSquared(number) {
+  if(!isNaN(number)) {
+    return number * number;
+  } else { // 2. If a non-number argument is passed into the function, alert NaN and prompt for another response.
+    alert("NaN");
+    let answer = numSquared(parseInt(prompt("That wasn't an actual number! What is your actual number?", "2")));
+    alert(answer);
+  }
+};
 
- // Takehome Day 3
+alert(answer);
 
- let myName = prompt('What is your name?');
- let favColor = prompt(`Ok ${myName}, so pick your favorite color from the following list and we'll tell you what the color is associated with. List of the colors to choose from: red, orange, yellow, green, blue, pink, purple, black, white, grey.`)
-  if(favColor.toLowerCase() === "red") {
-    alert(`Ok ${myName}, red is associated with energy, war, danger, strength, power, determination as well as passion, desire, and love.`);
-  } else if(favColor.toLowerCase() === "orange") {
-    alert(`Ok ${myName}, orange is associated with joy, sunshine, and the tropics.`)
-  } else if(favColor.toLowerCase() === "yellow") {
-    alert(`Ok ${myName}, yellow is associated with joy, happiness, intellect, and energy.`)
-  } else if(favColor.toLowerCase() === "green") {
-    alert(`Ok ${myName}, green is associated with growth, harmony, freshness, and fertility.`)
-  } else if(favColor.toLowerCase() === "blue") {
-    alert(`Ok ${myName}, blue is associated with depth, stability, tranquility, and calmness.`)
-  } else if(favColor.toLowerCase() === "pink") {
-    alert(`Ok ${myName}, pink is associated with romance, love, and friendship.`)
-  } else if(favColor.toLowerCase() === "purple") {
-    alert(`Ok ${myName}, purple is associated with wisdom, dignity, independence, creativity, mystery, and magic.`)
-  } else if(favColor.toLowerCase() === "black") {
-    alert(`Ok ${myName}, black is associated with power, elegance, formality, death, evil, and mystery.`)
-  } else if(favColor.toLowerCase() === "white") {
-    alert(`Ok ${myName}, white is associated with light, goodness, innocence, purity, and virginity.`)
-  } else if(favColor.toLowerCase() === "grey") {
-    alert(`Ok ${myName}, grey is associated with meanings of dull, dirty, and dingy, as well as formal, conservative, and sophisticated.`)
-  } else {
-    alert(`${myName}... You did not pick a color from the given list.`)
-  };
+// 3. In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period.
+
+function parseString(string) {
+  let parsedLetter = string.charAt(0).toUpperCase();
+  let stringArr = string.split('');
+  stringArr.splice(0, 1, parsedLetter);
+  string = stringArr.join(''); //Replaces the first letter with the new uppercased letter
+  if(string.charAt(string.length - 1) !== '.') string = string + '.';
+  return string;
+};
+
+// let answer = parseString(prompt("Type something in.", "i Heart cheese."));
+// alert(answer)
+
+// 4. Create a string that will flip the position of the first and second half of a string. For example, the string “abcdef” would be returned as “defabc”. Hint: use substring.
+
+function reverseHalves(string) {
+  let halfIndex = string.length / 2;
+  let firstHalf = string.substring(0, halfIndex);
+  let secondHalf = string.substring(halfIndex, string.length);
+  alert(`${secondHalf}${firstHalf}`);
+};
+
+// reverseHalves(prompt("give me a sentence", "hello world!"));
+
+// BONUS: If you are bored by all that, write a function that checks if a given string is a palindrome.
+
+function isPalindrome(string) {
+  let reversed = string
+                    .split('') //returns an array
+                    .reverse() //ONLY WORKS on arrays
+                    .join('')  //returns a string
+  return string === reversed;
+};
