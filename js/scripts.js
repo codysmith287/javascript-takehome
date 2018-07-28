bce93d31ed3190af9d5e7e888100156c655ea57b
Cody Smith <cms287@gmail.com>
2018-07-28 15:17:02 -0700

    fixed exclamation issue

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,23 @@
 
 const h3 = document.querySelector('#temp');
 
-let scale = prompt("Which is your preferred temperature scale(C or F)?", "C or F");
+let scale = prompt("Which is your preferred temperature scale(C or F)?", "F or C");
 
 let temp = prompt("What is the temperature? (only the number)", "99");
 
 // 2. Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
 
 function convertTemp(scale, temp) {
+  console.log(scale, temp);
   scale = scale.toLowerCase();
-  let returnValue;
-  let oppValue = scale === 'c' ? 'Fahrenheit' : 'Celcius';
+  let returnVal;
+  let oppositeVal = scale === 'c' ? 'Fahrenheit' : 'Celcius'; // Ternary statement ? is like if : is like else // If value is 'c' then the set opposite value is Fahrenehit, if is it "falsely" then the value is Celcius.
   temp = parseInt(temp);
-  if (!isNaN(temp)) {
-    returnValue = scale === 'c' ? (1.8 * temp) + 32 : (5 / 9) * (temp - 32);
+  if(!isNaN(temp)) {
+    returnVal = scale === 'c' ? (1.8 * temp) + 32 : (5 / 9) * (temp - 32); // Another statement, basically a condense if/else, best used for one single true false statment.
 
     // 3. Display the temperature in an h3 with minimal styling.
-    h3.innerText = `Your temperature is ${returnValue} ${oppValue}!`;
+    h3.innerText = `Your temperature is ${returnVal} ${oppositeVal}!`;
   } else {
     scale = prompt("Which is your preferred temperature scale(C or F)?", "C or F");
     temp = prompt("What is the temperature? (only the number)", "99");
@@ -33,8 +34,10 @@ convertTemp(scale, temp);
 // 4. From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
 // 5. Display the converted temperature in the h3 each time it is changed.
 h3.addEventListener('click', (e) => {
-  let valueArray = e.target.innerText.split(' ');
-  scale = valueArray[valueArray.length - 1] === 'Celcius' ? 'C' : 'F';
-  temp = valueArray[valueArray.length - 2];
+  let valueArr = e.target.innerText.split(' '); // split is how you take a string and turn it into an array.
+  console.log(valueArr[valueArr.length - 1]);
+  scale =
+  valueArr[valueArr.length - 1] === 'Celcius!' ? 'C' : 'F';
+  temp = valueArr[valueArr.length - 2];
   convertTemp(scale, temp);
-});
+}); // Research eventListeners further
